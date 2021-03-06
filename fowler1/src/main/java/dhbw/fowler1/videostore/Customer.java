5ba417c75a8b72d9d3371656edff0680f968e897
Simon Kritzer <simon.kritzer@hotmail.de>
2021-03-06 13:12:00 +0100

    Temporäre Variablen entfernt in Customer statement()

--- fowler1/src/main/java/dhbw/fowler1/videostore/Customer.java
+++ fowler1/src/main/java/dhbw/fowler1/videostore/Customer.java
@@ -20,26 +20,16 @@ public class Customer {
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
-            Rental each = (Rental) rentals.nextElement();
-            
-            // determine amount for each line
-            thisAmount = each.getCharge();
 
-            // add frequent renter points
-            frequentRenterPoints ++;
-            // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    each.getDaysRented() > 1) frequentRenterPoints ++;
+            Rental each = (Rental) rentals.nextElement();
 
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+                    String.valueOf(each.getCharge()) + "\n";
         }
         //add footer lines
-        result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) +
+        result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) +
                 " frequent renter points";
         return result;
     }
