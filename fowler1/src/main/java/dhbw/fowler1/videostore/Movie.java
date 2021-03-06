c68c11255571900cb8f076a99860dd5aedf891f0
Marco <marco.lichtenberger1@gmail.com>
2021-03-06 14:46:31 +0100

    getCharge() Aufrufe der Price Unterklassen in Movie

--- fowler1/src/main/java/dhbw/fowler1/videostore/Movie.java
+++ fowler1/src/main/java/dhbw/fowler1/videostore/Movie.java
@@ -4,43 +4,38 @@ public class Movie {
     public static final int CHILDRENS = 2;
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
-
+    private Price price;
     private String _title;
     private int _priceCode;
 
     public Movie(String title, int priceCode) {
         _title = title;
         _priceCode = priceCode;
+        switch (_priceCode){
+            case CHILDRENS:
+                setPrice(new ChildrensPrice());
+                break;
+            case REGULAR:
+                setPrice(new RegularPrice());
+                break;
+            case NEW_RELEASE:
+                setPrice(new NewReleasePrice());
+                break;
+        }
     }
 
-    public double getCharge (int daysRented) {
-        double result = 0;
-        switch (getPriceCode ()) {
-            case Movie.REGULAR:
-                result += 2;
-                if (daysRented > 2)
-                result += (daysRented- 2) * 1.5;
-                break;
-            case Movie.NEW_RELEASE:
-                result += daysRented * 3;
-                break;
-            case Movie.CHILDRENS:
-                result += 1.5;
-                if (daysRented> 3)
-                result += (daysRented- 3)* 1.5;
-                break;
-        }
-        return result;
+
+    public double getCharge(int daysRented) {
+        return price.getCharge(daysRented);
+    }
+    public int getFrequentRenterPoints(int daysRented) {
+        return price.getFrequentRenterPoints(daysRented);
+    }
+    void setPrice(Price newPrice) {
+        price = newPrice;
     }
 
-    public int getFrequentRenterPoints(int daysRented){
-        if((_priceCode == NEW_RELEASE) && daysRented > 1) {
-            return 2;
-        }
-        else {
-            return 1;
-        }
-    }
+
 
     public int getPriceCode() {
         return _priceCode;
